dd2106f6381e33470c5e40a615b008f691fb8375
avularadha <avularadha2001@gmail.com>
2026-06-19 23:41:08 +0530

    Remove API key

--- src/generator.py
+++ src/generator.py
@@ -1,11 +1,8 @@
-import os
-from dotenv import load_dotenv
+import streamlit as st
 from google import genai
 
-load_dotenv()
-
 client = genai.Client(
-    api_key=os.getenv("GEMINI_API_KEY")
+    api_key=st.secrets["GEMINI_API_KEY"]
 )
 
 
